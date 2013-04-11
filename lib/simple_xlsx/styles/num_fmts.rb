11f8517acb49532ace33cd0bdfd0445d46cf7915
Victor Bilyk <spy@undev.ru>
2013-04-11 14:59:16 +0700

    better number formats support

--- lib/simple_xlsx/styles/num_fmts.rb
+++ lib/simple_xlsx/styles/num_fmts.rb
@@ -15,20 +15,58 @@ module SimpleXlsx
       YYYYMMDD = 102
       YYYYMMDDHHMMSS = 103
 
+      def initialize
+        super
+        @taken_ids = {}
+        @next_id = 1000
+      end
+
+      def << o
+        if o.is_a? Fixnum
+          o
+        elsif o.is_a? String
+          add_num_fmt :format_code=>o
+        elsif o.is_a? Hash
+          add_num_fmt o
+        else
+          raise "Invalid number format, expected String, Hash or Fixnum."
+        end
+      end
+
       def to_stream stream
         stream.puts "<numFmts count=\"#{length}\">"
         @content.each{|(c, id)|
-          stream.puts "<numFmt formatCode=\"#{c[:format_code].to_s.to_xs}\" numFmtId=\"#{c[:id]}\"/>"
+          stream.puts "<numFmt formatCode=\"#{c[:format_code].to_s.to_xs}\" numFmtId=\"#{id}\"/>"
         }
         stream.puts "</numFmts>"
       end
 
       private
 
+      def add_num_fmt o
+        id = o[:id]
+        without_id = o.reject{|(k,v)| k == :id}
+
+        already_added_id = @content[without_id]
+        raise "Number format id #{id} already taken" if @taken_ids[id] && id != already_added_id
+
+        validate without_id
+
+        id ||= already_added_id
+        id ||= begin
+          r = @next_id
+          @next_id = @next_id + 1
+          r
+        end
+
+        @content[without_id] ||= id
+        @taken_ids[id] = true
+
+        id
+      end 
+
       def validate o
         super
-        raise ArgumentError, "No number format id specified" unless o[:id] 
-        raise ArgumentError, "Number format id should be a Fixnum" unless o[:id].is_a? Fixnum
         raise ArgumentError, "No number format code specified" unless o[:format_code]
       end
 
